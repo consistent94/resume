9fb0b9e09a066625cf1040142055b1349a145a17
Sebastian  Wilczega <sebastianwilczega@sebastians-mbp.home>
2024-11-05 22:46:34 +0000

    more styling

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/work", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "work.html"));
 });
 
 const PORT = process.env.PORT || 3000;
